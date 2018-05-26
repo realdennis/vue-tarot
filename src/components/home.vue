b29b8c8c79abab57572ef55498f8ed68b703ffb0
realdennis <evolon688@gmail.com>
2018-05-27 04:16:57 +0800

    offline when pwa(mode:standalone)

--- src/components/home.vue
+++ src/components/home.vue
@@ -5,7 +5,7 @@
       <h1> Tarobot </h1>
       <p>塔羅機器人</p>
     </div>
-    <divider>Let's Start From Here</divider>
+    <divider>{{ msg }}</divider>
     <div class="start">
       <x-button link="start" class="button" :gradients="['#FF2719', '#FF61AD']">開始使用</x-button>
       <x-button :link="{name:'result',params:{num:1}}" class="button" :gradients="['#FF2719', '#FF61AD']">直接抽一張</x-button>
@@ -19,6 +19,33 @@ import { XButton,Divider } from 'vux'
 export default {
   components: {
     XButton,Divider
+  },
+  data(){
+    return{
+      msg:"Let's Start From Here"
+    }
+  },
+  created(){
+    const offline = ()=>{
+      let s_result = require('./draw.js').s_result;
+      let d = s_result(78);
+      (async ()=>{
+        for(let i=0;i<78;i++){
+          let res = await fetch(d[i].path);
+        }
+      })();
+    };
+
+    if ('serviceWorker' in navigator){
+      if (window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone) {
+        offline();
+      }else{
+        this.msg = 'Add to home screen (using offline)';
+        //do something to tell user add to homescreen
+      }
+    }else{
+        //do not cache like a monkey...
+    }
   }
 }
 </script>
@@ -29,7 +56,7 @@ export default {
   margin-bottom: 20px;
 }
 .logo {
-  border-radius: 99px;
+  border-radius: 50%;
   width: 150px;
   height: 150px
 }
